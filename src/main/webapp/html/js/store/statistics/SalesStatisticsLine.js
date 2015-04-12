d6b490b1e65e35a9ac2f91eac4f2690603357dfc
Tang YunPeng <291662533@qq.com>
2015-04-12 23:03:45 +0800

    tongji

--- src/main/webapp/html/js/store/statistics/SalesStatisticsLine.js
+++ src/main/webapp/html/js/store/statistics/SalesStatisticsLine.js
@@ -1,6 +1,7 @@
 Ext.define('CRM.store.statistics.SalesStatisticsLine', {
     extend: 'Ext.data.Store',
     model: 'CRM.model.statistics.salesStatistics.SalesStatisticsLine',
+    autoLoad: false,
     proxy: {
         type: 'ajax',
         url: 'getCustomerCount.action',
--- src/main/webapp/html/js/view/statistics/sourceStatistics/SourceStatistics.js
+++ src/main/webapp/html/js/view/statistics/sourceStatistics/SourceStatistics.js
@@ -5,6 +5,7 @@ Ext.define('CRM.view.statistics.sourceStatistics.SourceStatistics', {
     id : 'sourcestatistics',
     layout : 'fit',
     initComponent : function() {
+        var me = this;
         this.statisticsStore = Ext.create('CRM.store.code.Code');
         this.statisticsTypeStore = Ext.create('CRM.store.code.Code');
         this.store = Ext.create('CRM.store.statistics.SalesStatisticsLine');
@@ -19,51 +20,102 @@ Ext.define('CRM.view.statistics.sourceStatistics.SourceStatistics', {
             }
         });
         this.items = [ {
-            xtype : 'chart',
-            style : 'background:#fff',
-            animate : true,
-            shadow : true,
-            store : this.store,
-            axes : [ {
-                type : 'Numeric',
-                position : 'left',
-                fields : [ 'count' ],
-                title : '个数',
-                grid : true,
-                minimum : 0
+
+            region: "center",
+            xtype: 'panel',
+            layout: 'card',
+            border: false,
+            activeItem: 0,
+            margins: '5 5 5 0',
+            id: 'statisticscard',
+            items: [ {
+                xtype : 'chart',
+                style : 'background:#fff',
+                animate : true,
+                shadow : true,
+                id : 'columncard',
+                store : this.store,
+                axes : [ {
+                    type : 'Numeric',
+                    position : 'left',
+                    fields : [ 'count' ],
+                    title : '个数',
+                    grid : true,
+                    minimum : 0
+                }, {
+                    type : 'Category',
+                    position : 'bottom',
+                    fields : [ 'name' ],
+                    id : 'Category'
+                } ],
+                series : [ {
+                    type : 'column',
+                    axis : 'left',
+                    highlight : true,
+                    tips : {
+                        trackMouse : true,
+                        width : 80,
+                        height : 28,
+                        renderer : function(storeItem, item) {
+                            this.setTitle(storeItem.get('name') + ': ' + storeItem.get('count'));
+                        }
+                    },
+                    label : {
+                        display : 'insideEnd',
+                        'text-anchor' : 'middle',
+                        field : 'count',
+                        orientation : 'vertical',
+                        color : '#333'
+                    },
+                    xField : 'name',
+                    yField : 'count'
+                } ]
             }, {
-                type : 'Category',
-                position : 'bottom',
-                fields : [ 'name' ],
-                title : '客户来源'
-            } ],
-            series : [ {
-                type : 'column',
-                axis : 'left',
-                highlight : true,
-                tips : {
-                    trackMouse : true,
-                    width : 80,
-                    height : 28,
-                    renderer : function(storeItem, item) {
-                        this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data'));
+                xtype: 'chart',
+                style: 'background:#fff',
+                animate: true,
+                shadow: true,
+                id : 'piecard',
+                legend: {
+                    position: 'right'
+                },
+                insetPadding: 60,
+                theme: 'Base:gradients',
+                store: this.store,
+                series: [ {
+                    type: 'pie',
+                    field: 'count',
+                    showInLegend: true,
+                    tips: {
+                        trackMouse: true,
+                        width: 140,
+                        height: 28,
+                        renderer: function(storeItem, item) {
+                            var total = 0;
+                            me.store.each(function(rec) {
+                                total += rec.get('count');
+                            });
+                            this.setTitle(storeItem.get('name') + ': ' + Math.round(storeItem.get('count') / total * 100) + '%');
+                        }
+                    },
+                    highlight: {
+                        segment: {
+                            margin: 20
+                        }
+                    },
+                    label: {
+                        field: 'name',
+                        display: 'rotate',
+                        contrast: true,
+                        font: '18px Arial'
                     }
-                },
-                label : {
-                    display : 'insideEnd',
-                    'text-anchor' : 'middle',
-                    field : 'count',
-                    orientation : 'vertical',
-                    color : '#333'
-                },
-                xField : 'name',
-                yField : 'count'
-            } ]
+                } ]
+            } ] 
         } ];
         this.tbar = [ {
             xtype : 'combobox',
             name : 'statisticsCombox',
-            labelWidth : 80,
+            labelWidth : 60,
             width : 200,
             fieldLabel : '统计方式',
             store : this.statisticsStore,
@@ -77,14 +129,14 @@ Ext.define('CRM.view.statistics.sourceStatistics.SourceStatistics', {
         }, {
             xtype : 'combobox',
             name : 'statisticsTypeCombox',
-            labelWidth : 80,
+            labelWidth : 60,
             width : 200,
             fieldLabel : '图形选择',
             store : this.statisticsTypeStore,
             displayField : 'value',
             valueField : 'code',
             itemId : 'statisticsTypeCombox',
-            margin : '0 0 0 15',
+            margin : '0 0 0 5',
             queryMode : 'local',
             editable : false,
             forceSelection : true,
@@ -92,6 +144,7 @@ Ext.define('CRM.view.statistics.sourceStatistics.SourceStatistics', {
         }, {
             xtype : 'button',
             text : '确定',
+            margin : '0 0 0 5',
             action : 'sourceStatisticsOk'
         } ];
         this.callParent(arguments);
